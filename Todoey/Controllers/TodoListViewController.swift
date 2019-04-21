15f1b26a69bb7fb4a587d8456f1c8ba452840387
Lal Sang <lalrams@hotmail.com>
2019-04-21 02:42:53 -0400

    messed up

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,17 +10,21 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray = ["1","2","3"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        if let items = UserDefaults.standard.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
         
+        let newItem = Item()
+        newItem.title = "4"
+        itemArray.append(newItem)
+        
+        // Do any additional setup after loading the view.
+//        if let items = UserDefaults.standard.array(forKey: "TodoListArray") as? [String] {
+//            itemArray = items
+//        }
         
     }
 
@@ -32,7 +36,7 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
         return cell
     }
     
@@ -60,7 +64,10 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            self.itemArray.append(textField.text!)
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Lal Sang on 4/21/19.
+//  Copyright © 2019 Lal Sang. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
